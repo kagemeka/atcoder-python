2625eb1d1f3139eab7d5454f4cd714d1e6c98c04
kagemeka <kagemeka1@gmail.com>
2022-01-24 13:16:04 +0900

    update pyproject.toml

---
+++ src/atcoder/__init__.py
@@ -0,0 +1,27 @@
+import platform
+import sys
+import typing
+
+print(platform.system())
+
+
+def main() -> None:
+    print(1)
+
+import typing 
+def main() -> None:
+    ...
+    
+standard_input = "hello world"
+
+
+def test():
+    a = sys.stdin.readline()
+    print(a)
+    # print(a)
+    print(2)
+
+
+if __name__ == "__main__":
+    test()
+    main()
